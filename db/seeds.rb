8f8876996b3f655a17680f9bd3585097d05519b0
Miriam Tocino <miriam.tocino@gmail.com>
2017-03-02 09:07:56 +0100

    Event Views
    - Update seeds
    - Create event index view
    - Create event show view
    - Create event form
    - Create new event view
    - Create event edit view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Event.delete_all
+User.delete_all
+
+miriam = User.create(email: "miriam@codaisseurup.com", password: "abcd1234")
+
+event = Event.create(name: "Monthly Yoga Weekend", description: "Let's come together and practice some asanas together in the woods!", location: "Amsterdam", includes_food: false, includes_drinks: true, price: 10.00, starts_at: 10.days.from_now, ends_at: 12.days.from_now, capacity: 100, active: true,  user: miriam)
