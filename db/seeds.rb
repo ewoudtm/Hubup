a1faf9af2043395b075b70a3e28c48867afee389
Miriam Tocino <miriam.tocino@gmail.com>
2017-03-11 09:43:48 +0100

    Add many-to-many association between event and category
    - Generate Category resource
    - Declare models associations
    - Update seeds with categories
    - List an event's categories
    - Add checkboxes to event form
    - Handle event form data
    
    Some extra improvements:
    
    - Add error messages in Event form
    - Style flash messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,38 @@
+Category.delete_all
 Event.delete_all
 User.delete_all
 
-miriam = User.create(email: "miriam@codaisseurup.com", password: "abcd1234")
+miriam = User.create!(
+  email: "miriam@codaisseurup.com",
+  password: "abcd1234",
+)
 
-event = Event.create(name: "Monthly Yoga Weekend", description: "Let's come together and practice some asanas together in the woods!", location: "Amsterdam", includes_food: false, includes_drinks: true, price: 10.00, starts_at: 10.days.from_now, ends_at: 12.days.from_now, capacity: 100, active: true,  user: miriam)
+event = Event.create!(
+  name: "Monthly Yoga Weekend",
+  description: "Let's come together and practice some asanas together in the woods!",
+  location: "Amsterdam",
+  includes_food: false,
+  includes_drinks: true,
+  price: 10.00,
+  starts_at: 10.days.from_now,
+  ends_at: 12.days.from_now,
+  capacity: 100,
+  active: true,
+  user: miriam,
+)
+
+Category.create!([
+  { name: "Movements" },
+  { name: "Outdoors & Adventure" },
+  { name: "Tech" },
+  { name: "Family" },
+  { name: "Health & Wellness" },
+  { name: "Sports & Fitness" },
+  { name: "Learning" },
+  { name: "Photography" },
+  { name: "Food & Drink" },
+])
+
+event = Event.find_by(name: "Monthly Yoga Weekend")
+event.categories << Category.find_by(name: "Outdoors & Adventure")
+event.categories << Category.find_by(name: "Sports & Fitness")
