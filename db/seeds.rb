4a90407c5723c1637acfc382ee0acfac4db44fc6
ewoudtm <ewoud@ewoudtm.nl>
2017-03-15 23:58:17 +0100

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
 Category.delete_all
 Event.delete_all
+Profile.delete_all
 User.delete_all
 
 miriam = User.create!(
-  email: "miriam@codaisseurup.com",
+  email: "miriam@hubup.com",
   password: "abcd1234",
 )
 
@@ -18,10 +19,10 @@ event = Event.create!(
   ends_at: 12.days.from_now,
   capacity: 100,
   active: true,
-  user: miriam,
+  user: miriam
 )
 
-Category.create!([
+theme = Category.create!([
   { name: "Movements" },
   { name: "Outdoors & Adventure" },
   { name: "Tech" },
@@ -30,9 +31,49 @@ Category.create!([
   { name: "Sports & Fitness" },
   { name: "Learning" },
   { name: "Photography" },
-  { name: "Food & Drink" },
+  { name: "Food & Drink" }
 ])
 
 event = Event.find_by(name: "Monthly Yoga Weekend")
 event.categories << Category.find_by(name: "Outdoors & Adventure")
 event.categories << Category.find_by(name: "Sports & Fitness")
+
+
+for i in 1..6 do
+  first_name = Faker::Name.first_name
+  user = User.create!(
+  email: "#{first_name}@hubup.com",
+  password: "abcd1234"
+  )
+
+  profile = Profile.create!(
+  first_name: first_name,
+  last_name: Faker::Name.last_name,
+  bio: Faker::ChuckNorris.fact
+  )
+
+  for x in 1..(Faker::Number.between(2,4)) do
+    event = Event.create!(
+    name: Faker::Company.buzzword,
+    description: Faker::Company.bs,
+    location: "Amsterdam",
+    includes_food: false,
+    includes_drinks: true,
+    price: Faker::Commerce.price,
+    starts_at: Date.today.next_day(Faker::Number.between(5,10)),
+    ends_at: Date.today.next_day(Faker::Number.between(11,45)),
+    capacity: (Faker::Number.between(1, 10))*5,
+    active: true,
+    user: user
+    )
+
+    event.categories << Category.find_by(name: "Outdoors & Adventure")
+    event.categories << Category.find_by(name: "Sports & Fitness")
+
+    # for y in 1..(Faker::Number.between(1, 3)) do
+    #   event.categories << theme[Faker::Number.between(1, 9)]
+    # end
+    # Faker::Number.unique.clear
+  end
+
+end
